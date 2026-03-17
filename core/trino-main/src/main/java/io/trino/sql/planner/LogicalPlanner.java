e74511ca26f29ff6c6744b5857670cd539d18480
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2026-03-17 14:25:45 +0530

    fixup! Detect materialized views with non-deterministic functions as stale

--- core/trino-main/src/main/java/io/trino/sql/planner/LogicalPlanner.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/LogicalPlanner.java
@@ -59,7 +59,6 @@ import io.trino.spi.type.Type;
 import io.trino.spi.type.VarcharType;
 import io.trino.sql.PlannerContext;
 import io.trino.sql.analyzer.Analysis;
-import io.trino.sql.analyzer.ExpressionTreeUtils;
 import io.trino.sql.analyzer.Field;
 import io.trino.sql.analyzer.RelationId;
 import io.trino.sql.analyzer.RelationType;
@@ -96,15 +95,10 @@ import io.trino.sql.planner.planprinter.PlanPrinter;
 import io.trino.sql.planner.sanity.PlanSanityChecker;
 import io.trino.sql.tree.Analyze;
 import io.trino.sql.tree.CreateTableAsSelect;
-import io.trino.sql.tree.CurrentDate;
-import io.trino.sql.tree.CurrentTime;
-import io.trino.sql.tree.CurrentTimestamp;
 import io.trino.sql.tree.Delete;
 import io.trino.sql.tree.ExplainAnalyze;
 import io.trino.sql.tree.Insert;
 import io.trino.sql.tree.LambdaArgumentDeclaration;
-import io.trino.sql.tree.LocalTime;
-import io.trino.sql.tree.LocalTimestamp;
 import io.trino.sql.tree.Merge;
 import io.trino.sql.tree.NodeRef;
 import io.trino.sql.tree.Query;
@@ -151,6 +145,7 @@ import static io.trino.spi.type.BooleanType.BOOLEAN;
 import static io.trino.spi.type.IntegerType.INTEGER;
 import static io.trino.spi.type.VarbinaryType.VARBINARY;
 import static io.trino.spi.type.VarcharType.VARCHAR;
+import static io.trino.sql.analyzer.DeterminismEvaluator.containsCurrentTimeFunctions;
 import static io.trino.sql.analyzer.SemanticExceptions.semanticException;
 import static io.trino.sql.analyzer.TypeSignatureProvider.fromTypes;
 import static io.trino.sql.ir.Booleans.TRUE;
@@ -696,11 +691,7 @@ public class LogicalPlanner
                 .map(polymorphicTableFunction -> polymorphicTableFunction.getNode().getName().toString())
                 .collect(toImmutableList());
         boolean hasNonDeterministicFunctions = analysis.getResolvedFunctions().stream().anyMatch(function -> !function.deterministic())
-                || !ExpressionTreeUtils.extractExpressions(ImmutableList.of(query), CurrentDate.class).isEmpty()
-                || !ExpressionTreeUtils.extractExpressions(ImmutableList.of(query), CurrentTime.class).isEmpty()
-                || !ExpressionTreeUtils.extractExpressions(ImmutableList.of(query), CurrentTimestamp.class).isEmpty()
-                || !ExpressionTreeUtils.extractExpressions(ImmutableList.of(query), LocalTime.class).isEmpty()
-                || !ExpressionTreeUtils.extractExpressions(ImmutableList.of(query), LocalTimestamp.class).isEmpty();
+                || containsCurrentTimeFunctions(query);
         RefreshMaterializedViewReference writerTarget = new RefreshMaterializedViewReference(
                 viewAnalysis.getTable().toString(),
                 tableHandle,
